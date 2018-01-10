c3e08e4f9fe8d28521978e61d8bcb0574ec3a330
DESKTOP-5GKGLEP\Mark <shlukiman@gmail.com>
2018-01-10 10:45:06 -0600

    First commit

---
+++ Assets/Scripts/GUIManager.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GUIManager : MonoBehaviour {
+
+    public Text centerMessage;
+    public Text BalanceText;
+    //private bool showRestart = false;
+
+    public GameObject IntroObject;
+    public GameObject ActionsObject;
+    public GameObject RestartButton;
+
+    public static GUIManager Instance;
+
+    void Start()
+    {
+        Instance = this;
+    }
+
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            if (Input.touchCount > 0)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    print(hit.transform.tag);
+                    //SceneManager.LoadScene(1);
+                    if(!hit.transform.tag.Equals("Untagged"))
+                        GameManager.Instance.LoadMainStage(hit.transform.tag);
+                }
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    print(hit.transform.tag);
+                    //SceneManager.LoadScene(1);
+                    if (!hit.transform.tag.Equals("Untagged"))
+                        GameManager.Instance.LoadMainStage(hit.transform.tag);
+                }
+            }
+        }
+    }
+
+
+    public void UpdateBalance()
+    {
+        BalanceText.text = "Balance: " + GameManager.Instance.PlayerBalance + "$";
+    }
+
+    public void ShowPlayerLostMessage()
+    {
+        RestartButton.SetActive(true);
+        ActionsObject.SetActive(false);
+        centerMessage.text = "PLAYER LOST";
+    }
+
+    public void ShowPlayerWonMessage()
+    {
+        RestartButton.SetActive(true);
+        ActionsObject.SetActive(false);
+        centerMessage.text = "PLAYER WIN!!!";
+    }
+
+    public void ShowTie()
+    {
+        RestartButton.SetActive(true);
+        ActionsObject.SetActive(false);
+        centerMessage.text = "TIE";
+    }
+
+
+
+    public void Hit()
+    {
+        GameManager.Instance.Hit();
+    }
+
+    public void Stand()
+    {
+        GameManager.Instance.Stand();
+    }
+
+    public void Double()
+    {
+        GameManager.Instance.Double();
+    }
+
+    public void Surrender()
+    {
+        GameManager.Instance.Surrender();
+    }
+
+    public void Restart()
+    {
+        RestartButton.SetActive(false);
+        ActionsObject.SetActive(true);
+        GameManager.Instance.Restart();
+        centerMessage.text = "";
+    }
+
+    public void ShowActionButtons()
+    {
+        ActionsObject.SetActive(true);
+    }
+
+    public void ARTargetFound()
+    {
+        IntroObject.SetActive(false);
+        ActionsObject.SetActive(true);
+        BalanceText.gameObject.SetActive(true);
+    }
+
+    public void ARTargetLost()
+    {
+        IntroObject.SetActive(true);
+        ActionsObject.SetActive(false);
+    }
+
+
+    /*
+    void OnGUI()
+    {
+        if (!showRestart)
+        {
+            if (GUI.Button((new Rect(0, 0, 200, 100)), "HIT"))
+            {
+                GameManager.Instance.Hit();
+            }
+            if (GUI.Button((new Rect(200, 0, 200, 100)), "Stand"))
+            {
+                GameManager.Instance.Stand();
+            }
+            if (GUI.Button((new Rect(400, 0, 200, 100)), "Double"))
+            {
+                GameManager.Instance.Double();
+            }
+            if (GUI.Button((new Rect(600, 0, 200, 100)), "Surrender"))
+            {
+                GameManager.Instance.Surrender();
+            }
+        }
+        else
+        {
+            if (GUI.Button((new Rect(600, 0, 200, 100)), "Bet 10$"))
+            {
+                showRestart = false;
+                GameManager.Instance.Restart();
+                centerMessage.text = "";
+            }
+        }
+        
+    }*/
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,240 @@
+﻿using Blackjack.Common;
+using Blackjack.GameManager;
+using Blackjack.Players;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameManager : MonoBehaviour {
+
+    private Manager logicManager;
+    public Player player;
+
+
+    public GameObject GameHolder;
+    public GameObject ChooseBetHolder;
+    public GameObject CardPrefab;
+    public GameObject PlayerHandHolder;
+    public GameObject DealerHandHolder;
+    public GameObject WinParticles;
+
+    private List<GameObject> cardRefs;
+    private GameObject downfacedCard;
+
+    private int cardCount = 0;
+    private int cardCountDealer = 0;
+
+    private int betAmount = 10;
+
+    private int playerBalance;
+    public int PlayerBalance
+    {
+        get
+        {
+            return playerBalance;
+        }
+
+        set
+        {
+            playerBalance = value;
+        }
+    }
+    [HideInInspector]
+    public static GameManager Instance;
+
+    
+
+    // Use this for initialization
+    void Start ()
+    {
+        Instance = this;
+        this.player = new Player();
+        logicManager = new Manager(player);
+        cardRefs = new List<GameObject>();
+
+        PlayerBalance = 500;
+        
+
+        downfacedCard = null;
+
+        logicManager.DealFirstTwoCards();
+        Debug.Log("Players hand:\n" + player.ShoutHand());
+
+        
+    }
+
+    private IEnumerator PlaceBegginingCards()
+    {
+        List<Card> hand = player.ShowHand();
+        for (int i = 0; i < hand.Count; i++)
+        {
+            PlaceCard(hand[i], PlayerHandHolder, true, false);
+            yield return new WaitForSeconds(1);
+        }
+
+        List<Card> hand2 = logicManager.getDealerCards();
+        for (int i = 0; i < hand.Count; i++)
+        {
+            PlaceCard(hand2[i], DealerHandHolder, false, i == 1);
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+
+    public void LoadMainStage(string betTag)
+    {
+        GUIManager.Instance.UpdateBalance();
+        GUIManager.Instance.ShowActionButtons();
+
+        string str = betTag.Substring(4);
+        betAmount = int.Parse(str);
+
+        ChooseBetHolder.SetActive(false);
+        GameHolder.SetActive(true);
+
+        StartCoroutine(PlaceBegginingCards());
+    }
+
+    private void PlaceCard(Card logicCard, GameObject pivot, bool isPlayerCard, bool isFacedDown)
+    {
+        GameObject cardHolder = Instantiate(new GameObject(), pivot.transform);
+        GameObject card = Instantiate(CardPrefab, cardHolder.transform);
+
+        if (isPlayerCard)
+        {
+            cardHolder.transform.localPosition = new Vector3(cardCount * 0.2f, 0.01f + cardCount * 0.01f, 0);
+        }
+        else
+        {
+            cardHolder.transform.localPosition = new Vector3(cardCountDealer * 0.2f, 0.01f + cardCountDealer * 0.01f, 0);
+            cardHolder.transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+        string textureName = "PNG Cards/" + (logicCard.Value + "_of_" + logicCard.Suit).ToLower();
+        Debug.Log("texture name: " + textureName);
+        Texture tex = Resources.Load<Texture>(textureName);
+        card.GetComponent<Renderer>().material.mainTexture = tex;
+
+        if (isFacedDown)
+        {
+            cardHolder.transform.eulerAngles = new Vector3(0, cardHolder.transform.eulerAngles.y, 180);
+            downfacedCard = cardHolder;
+        }
+
+        cardRefs.Add(card);
+
+        card.GetComponent<Animator>().enabled = true;
+
+        if (isPlayerCard)
+            cardCount++;
+        else
+            cardCountDealer++;
+    }
+
+    public void FlipDownfacedCard()
+    {
+        downfacedCard.transform.eulerAngles = Vector3.zero;
+    }
+
+
+    public void Restart()
+    {
+        //discard from old
+        foreach (var card in cardRefs)
+        {
+            Destroy(card);
+        }
+        cardCount = 0;
+        cardCountDealer = 0;
+        downfacedCard = null;
+        WinParticles.SetActive(false);
+
+        //set new
+        playerBalance -= betAmount;
+        GUIManager.Instance.UpdateBalance();
+
+        logicManager.StartNewDeal();
+        /*List<Card> hand = player.ShowHand();
+        for (int i = 0; i < hand.Count; i++)
+        {
+            PlaceCard(hand[i], PlayerHandHolder, true, false);
+        }
+
+        List<Card> hand2 = logicManager.getDealerCards();
+        for (int i = 0; i < hand.Count; i++)
+        {
+            PlaceCard(hand2[i], DealerHandHolder, false, i == 1);
+        }*/
+
+        StartCoroutine(PlaceBegginingCards());
+    }
+
+    public void Hit()
+    {
+        Debug.Log("HIT");
+        player.HitMe(logicManager.DealCard());
+
+        PlaceCard(player.LastCard(), PlayerHandHolder, true, false);
+
+        if (player.GetScore() > 21)
+        {
+            //Player lost
+            GUIManager.Instance.ShowPlayerLostMessage();
+        }
+    }
+
+    public void Stand()
+    {
+        Debug.Log("stand");
+
+        FlipDownfacedCard();
+
+        while(logicManager.GetDealerScore() <= 16)
+        {
+            logicManager.GiveDealerACard();
+            PlaceCard(logicManager.DealerLastCard, DealerHandHolder, false, false);
+        }
+
+        if (logicManager.GetDealerScore() > 21)
+        {
+            WinParticles.SetActive(true);
+            GUIManager.Instance.ShowPlayerWonMessage();
+            playerBalance += betAmount * 2;
+            GUIManager.Instance.UpdateBalance();
+        }
+        else
+        {
+            if (player.GetScore() > logicManager.GetDealerScore())
+            {
+                WinParticles.SetActive(true);
+                GUIManager.Instance.ShowPlayerWonMessage();
+                playerBalance += betAmount * 2;
+                GUIManager.Instance.UpdateBalance();
+            }
+            else if (player.GetScore() == logicManager.GetDealerScore())
+            {
+                GUIManager.Instance.ShowTie();
+                playerBalance += betAmount;
+                GUIManager.Instance.UpdateBalance();
+            }
+            else
+            {
+                GUIManager.Instance.ShowPlayerLostMessage();
+            }
+        }
+    }
+
+    public void Double()
+    {
+        Debug.Log("double");
+        Hit();
+        Stand();
+    }
+
+    public void Surrender()
+    {
+        Restart();
+    }
+
+}
